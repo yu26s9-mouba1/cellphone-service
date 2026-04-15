5c49019d6b048aa8593d608b314929d8f8fb9520
Moubarak Congacou <cmouba1999@gmail.com>
2026-04-15 11:33:53 -0400

    I have began creating the new phone

--- src/main/java/com/pluralsight/CellPhone.java
+++ src/main/java/com/pluralsight/CellPhone.java
@@ -45,7 +45,7 @@ public class CellPhone {
         return owner;
     }
 
-    
+
 }
 
 
--- src/main/java/com/pluralsight/CellPhoneApplication.java
+++ src/main/java/com/pluralsight/CellPhoneApplication.java
@@ -1,7 +1,16 @@
 package com.pluralsight;
+import java.util.Scanner;
 
 public class CellPhoneApplication {
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        // Now that I have setted the cellphone parametters, i can create the object which is the cellphone
+
+        CellPhone phone = new CellPhone(); // This will allow the object to be built from memory
+
+        
+
 
     }
 
