4fafc77aa8769486a152ea1609c83c97f0720d20
Moubarak Congacou <cmouba1999@gmail.com>
2026-04-15 11:11:32 -0400

    I have just created variables for the cellphone properties

--- src/main/java/com/pluralsight/CellPhone.java
+++ src/main/java/com/pluralsight/CellPhone.java
@@ -1,5 +1,15 @@
 package com.pluralsight;
 
 public class CellPhone {
-    
+
+    //Defining variables for the cellphone data or properties
+    private int SerialNumber;
+    private String model;
+    private String carrier;
+    private String phoneNumber;
+    private String owner;
+
+
 }
+
+
