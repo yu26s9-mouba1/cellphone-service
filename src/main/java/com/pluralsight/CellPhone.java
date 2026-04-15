9bac12fbe86fa12dae1f702c5c44f0e49cdcd97b
Moubarak Congacou <cmouba1999@gmail.com>
2026-04-15 11:29:10 -0400

    I have added the getters and add setters to cahnge the data

--- src/main/java/com/pluralsight/CellPhone.java
+++ src/main/java/com/pluralsight/CellPhone.java
@@ -10,6 +10,42 @@ public class CellPhone {
     private String owner;
 
 
+    // Creating the constructor operator
+    public CellPhone() {
+
+        this.SerialNumber = 0;
+        this.model = "";
+        this.carrier = "";
+        this.phoneNumber = "";
+        this.owner = "";
+
+
+    }
+
+//Creating or adding getters that will get data from the parameter constructor
+
+    public int getSerialNumber() {
+        return SerialNumber;
+
+    }
+
+    public String getmodel() {
+        return model;
+    }
+
+    public String getCarrier() {
+        return carrier;
+    }
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public String getowner() {
+        return owner;
+    }
+
+    
 }
 
 
