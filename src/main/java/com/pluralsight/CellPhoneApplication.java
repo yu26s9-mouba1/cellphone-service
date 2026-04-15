5a32388aa67eab5309707906b2a80b0cc72ff2ea
Moubarak Congacou <cmouba1999@gmail.com>
2026-04-15 11:39:22 -0400

    I have just prompted the user for details about the phone to be able the detect what specific phone is it

--- src/main/java/com/pluralsight/CellPhoneApplication.java
+++ src/main/java/com/pluralsight/CellPhoneApplication.java
@@ -9,7 +9,44 @@ public class CellPhoneApplication {
 
         CellPhone phone = new CellPhone(); // This will allow the object to be built from memory
 
+        // Prompting the user for the serial number
+
+        System.out.print("What is the serial number? ");
+        int serial = scanner.nextInt();
+        scanner.nextLine(); //Making it clears the buffer after user input
+        phone.setSerialNumber(serial);
+
+        //Prompting the user for the phone model
+
+        System.out.print("What model is the phone? ");
+        String model = scanner.nextLine();
+        phone.setModel(model);
+
+
+        //Prompting the user for the phone carrier
+
+        System.out.print("Who is the carrier? ");
+        String carrier = scanner.nextLine();
+        phone.setCarrier(carrier);
+
+        //Prompting the user for the phone number
+        System.out.print("What is the phone number? ");
+        String number = scanner.nextLine();
+        phone.setPhoneNumber(number);
+
+
+        ////Prompting the user for the phone owner
+        System.out.print("Who is the owner of the phone? ");
+        String owner = scanner.nextLine();
+        phone.setOwner(owner);
+
+
         
+
+
+
+
+
 
 
     }
